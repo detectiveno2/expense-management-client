6e2aca480eb81edccb8c0a8554b549b537d007dd
nguynhieu <tthzicc@gmail.com>
2020-08-15 21:45:11 +0700

    fix response function LoginFacebook

--- src/components/FacebookLogin/FacebookLogin.js
+++ src/components/FacebookLogin/FacebookLogin.js
@@ -1,23 +1,23 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import FacebookLogin from 'react-facebook-login';
+import axios from 'axios';
 
 import { ReactComponent as FacebookIcon } from '../../images/facebook.svg';
 
+import ENDPOINT from '../../ENDPOINT';
 import './FacebookLogin.css';
 
 export default function () {
-	const [isLoggedIn, setIsLoggedIn] = useState(false);
-	const [userID, setUserID] = useState('');
-	const [name, setName] = useState('');
-	const [email, setEmail] = useState('');
-	const [picture, setPicture] = useState('');
-
-	// const componentClicked = () => {
-	// 	console.log('clicked');
-	// };
+	const [err, setErr] = useState(null);
 
 	const responseFacebook = (response) => {
-		console.log(response);
+		axios
+			.post(`${ENDPOINT}/api/auth/facebook`, {
+				userName: response.name,
+				userID: response.userID,
+			})
+			.then((res) => console.log(res))
+			.catch((err) => setErr(err.response.data));
 	};
 
 	return (
@@ -25,7 +25,6 @@ export default function () {
 			appId="747911565967671"
 			autoLoad={true}
 			fields="name,email,picture"
-			// onClick={componentClicked}
 			callback={responseFacebook}
 			textButton={
 				<span>
