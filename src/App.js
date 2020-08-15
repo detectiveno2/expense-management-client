6de33fdb4addee7633a0a363f1fc6219cfb266e7
nguynhieu <tthzicc@gmail.com>
2020-08-15 14:30:29 +0700

    Build login UI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch } from "react-router-dom";
+
+import PublicRoute from "./routes/PublicRoute";
+
+import Login from "./pages/Login/Login";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="wrapper">
+        <div className="main wrap-content">
+          <Switch>
+            <PublicRoute exact path="/login" component={Login} />
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 }
 
