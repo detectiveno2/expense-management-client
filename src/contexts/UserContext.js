e773895bf7826f17e01cbe67aca0519e4fb71c0a
tuan trung <tranvantuantrung@gmail.com>
2020-08-21 18:46:30 +0700

    fix user is not json

--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -3,7 +3,16 @@ import React, { useState } from 'react';
 export const UserContext = React.createContext();
 
 export const UserProvider = (props) => {
-	const user = JSON.parse(localStorage.getItem('user'));
+	let user;
+	try {
+		user = JSON.parse(localStorage.getItem('user'));
+	} catch (err) {
+		if (err) {
+			localStorage.removeItem('authToken');
+			localStorage.removeItem('user');
+		}
+	}
+
 	const [token, setToken] = useState(null);
 	const [currentUser, setCurrentUser] = useState(user);
 
