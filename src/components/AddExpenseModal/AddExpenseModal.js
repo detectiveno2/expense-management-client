df1bc4438bc1135dc1e12326b5857036f44218a2
detectiveno2 <nhatthien185@gmail.com>
2020-08-21 17:57:55 +0700

    Complete add expense feature in client-side

--- src/components/AddExpenseModal/AddExpenseModal.js
+++ src/components/AddExpenseModal/AddExpenseModal.js
@@ -1,23 +1,77 @@
 import React, { useState } from 'react';
 import { Modal, Button } from 'antd';
+import moment from 'moment';
+import swal from 'sweetalert';
 
+import ExpenseModal from '../ExpenseModal/ExpenseModal';
 import './AddExpenseModal.css';
 
 function AddExpenseModal() {
+	const currentDate = moment();
+
+	const [date, setDate] = useState(currentDate);
+	const [wallet, setWallet] = useState(null);
+	const [isIncome, setIsIncome] = useState(false);
+	const [expense, setExpense] = useState('');
+	const [description, setDescription] = useState('');
+
 	const [visible, setVisible] = useState(false);
 	const [loading, setLoading] = useState(false);
 
-	const showModal = () => {
-		setVisible(true);
-	};
+	// Functions for UI.
 
-	const handleOk = () => {
+	const handleOk = async () => {
+		const strError = 'Bạn vui lòng điền đầy đủ ngày/số tiền giao dịch.';
 		setLoading(true);
 
-		setTimeout(() => {
-			setVisible(false);
+		// Validate on client side.
+		if (!date || !expense) {
+			swal({
+				text: strError,
+				title: 'Lỗi!',
+				icon: 'warning',
+			});
 			setLoading(false);
-		}, 1500);
+		}
+	};
+
+	// Helper functions.
+	const changeDateSelect = (date, dateString) => {
+		console.log(date, dateString);
+		setDate(date);
+	};
+
+	const changeWalletSelect = (wallet) => {
+		console.log(`selected ${wallet}`);
+		setWallet(wallet);
+	};
+
+	const changeTypeSelect = (type) => {
+		const isIncome = type || false;
+		console.log(isIncome);
+		setIsIncome(isIncome);
+	};
+
+	const changeExpenseSelect = (event) => {
+		if (event.target.value === '') {
+			setExpense('');
+			return;
+		}
+
+		const inputExpense = event.target.validity.valid
+			? event.target.value
+			: expense;
+
+		setExpense(inputExpense);
+	};
+
+	const handleChangeDescription = (event) => {
+		const description = event.target.value;
+		setDescription(description);
+	};
+
+	const showModal = () => {
+		setVisible(true);
 	};
 
 	const handleCancel = () => {
@@ -30,7 +84,7 @@ function AddExpenseModal() {
 				THÊM GIAO DỊCH
 			</Button>
 			<Modal
-				title="Basic Modal"
+				title="Thêm giao dịch"
 				visible={visible}
 				onCancel={handleCancel}
 				footer={[
@@ -47,9 +101,15 @@ function AddExpenseModal() {
 					</Button>,
 				]}
 			>
-				<p>Some contents...</p>
-				<p>Some contents...</p>
-				<p>Some contents...</p>
+				<ExpenseModal
+					expense={expense}
+					description={description}
+					changeDateSelect={changeDateSelect}
+					changeWalletSelect={changeWalletSelect}
+					changeExpenseSelect={changeExpenseSelect}
+					changeTypeSelect={changeTypeSelect}
+					handleChangeDescription={handleChangeDescription}
+				/>
 			</Modal>
 		</>
 	);
---
+++ src/components/ExpenseModal/ExpenseModal.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import { DatePicker, Space, Select, Input } from 'antd';
+import moment from 'moment';
+
+import './ExpenseModal.css';
+
+function ExpenseModal(props) {
+	// Get current date.
+	const currentDate = moment();
+
+	const {
+		expense,
+		description,
+		changeDateSelect,
+		changeWalletSelect,
+		changeTypeSelect,
+		changeExpenseSelect,
+		handleChangeDescription,
+	} = props;
+
+	const { Option } = Select;
+
+	return (
+		<div className="ExpenseModal">
+			<div className="FlexLayout">
+				<Space direction="vertical">
+					<DatePicker
+						style={{ width: '100%' }}
+						defaultValue={currentDate}
+						onChange={changeDateSelect}
+						format="DD/MM/YYYY"
+					/>
+				</Space>
+				<Select defaultValue="lucy" onChange={changeWalletSelect}>
+					<Option value="jack">Vi den</Option>
+					<Option value="lucy">Vi do</Option>
+					<Option value="Yiminghe">Vi xanh</Option>
+				</Select>
+				<Select defaultValue="false" onChange={changeTypeSelect}>
+					<Option value="false">Khoản chi</Option>
+					<Option value="true">Khoản thu</Option>
+				</Select>
+			</div>
+			<Input
+				type="text"
+				pattern="[0-9]*"
+				placeholder="Số tiền"
+				maxLength="23"
+				value={expense}
+				onChange={changeExpenseSelect}
+				required
+			/>
+			<Input
+				type="text"
+				placeholder="Ghi chú"
+				value={description}
+				onChange={handleChangeDescription}
+			/>
+		</div>
+	);
+}
+
+export default ExpenseModal;
