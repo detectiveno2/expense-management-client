35459a8dcda696eee2c0f4fa2cc8c7442e60b3ea
detectiveno2 <nhatthien185@gmail.com>
2020-08-26 20:46:02 +0700

    add wallet api

--- src/api/walletApi.js
+++ src/api/walletApi.js
@@ -5,6 +5,11 @@ const walletApi = {
 		const url = '/wallet';
 		return axiosClient.get(url);
 	},
+
+	add: (data) => {
+		const url = '/wallet/add';
+		return axiosClient.post(url, data);
+	},
 };
 
 export default walletApi;
