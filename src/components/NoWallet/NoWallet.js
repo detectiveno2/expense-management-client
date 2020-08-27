04c85c50146319eccf5176aab5fb324c0fee7518
detectiveno2 <nhatthien185@gmail.com>
2020-08-27 14:13:57 +0700

    Fix main green color

--- src/components/NoWallet/NoWallet.js
+++ src/components/NoWallet/NoWallet.js
@@ -6,8 +6,7 @@ function NoWallet() {
 	return (
 		<div className="no-wallet">
 			<p className="no-wallet__title">
-				Bạn chưa có ví nào, hãy tạo ít nhất cho mình một ví trước khi bắt đầu
-				:-)
+				Bạn chưa có ví nào, hãy tạo ít nhất cho mình một ví trước khi bắt đầu.
 			</p>
 			<button className="no-wallet__add-btn">Tạo ví</button>
 		</div>
